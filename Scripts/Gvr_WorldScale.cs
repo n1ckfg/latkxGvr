4f925db45986e73e95ef6b3f6701a758e634881a
Nick Fox-Gieg <nick@fox-gieg.com>
2020-03-10 10:55:27 -0400

    rename worldscale

--- Scripts/Gvr_WorldScale.cs
+++ Scripts/Gvr_WorldScale.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 // https://www.reddit.com/r/Unity3D/comments/a6rmbh/vr_question_steamvr_2_scale_object_with_both_hands/
 
-public class Gvr_SevenMove : MonoBehaviour
+public class Gvr_WorldScale : MonoBehaviour
 {
 
     public Gvr_NewController steamCtlMain;
